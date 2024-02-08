0eade213498cb58fb0f1c685e33b2081a2436e65
Cangcorangsz <bebekkeren1234@gmail.com>
2024-02-08 08:27:58 +0700

    fix: Memperbaiki path logo

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
         <header className='w-full bg-slate-50 fixed drop-shadow-md top-0 z-10'>
             <nav className='flex items-center justify-between container mx-auto px-2'>
                 <a href='#'>
-                    <img src="/logo.png" className='w-16 md:w-12' alt="" />
+                    <img src="./logo.png" className='w-16 md:w-12' alt="" />
                 </a>
                 <div className='flex gap-x-4'>
                     <HamburgerMenu showMenu={showMenu} onClick={() => setShowMenu(!showMenu)} />
